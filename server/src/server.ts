a7403effb549a4062f75e694ede8a29fa389d579
Sara <lokifeathers@gmail.com>
2025-02-10 21:00:46 -0500

    Remove module type declaration from package.json and comment out __dirname definition in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 import db from './config/connection.js';
 
 // const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
