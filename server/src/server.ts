b5ca4b9ba46bcf848b64bc6fcf1e880d11655619
Sara <lokifeathers@gmail.com>
2025-02-10 20:52:18 -0500

    Add initial client and server files, including components, configuration, and environment setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,21 @@
 import express from 'express';
 import path from 'node:path';
 
-//import db from './config/connection.js';
+import db from './config/connection.js';
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
 
 const startApolloServer = async () => {
-    await server.start();
+    // await server.start();
     console.log('Apollo Server started');
 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-
-    app.use('/graphql', expressMiddleware(server, {
-        context: authMiddleware
-    }));
 
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../../client/dist')));
