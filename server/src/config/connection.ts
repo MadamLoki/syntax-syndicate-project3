ba04d1cfac6a72bfdde97f790faaac3c88b94acc
Sara <lokifeathers@gmail.com>
2025-02-10 21:09:52 -0500

    Comment out MongoDB connection in connection.ts for easier configuration management

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/petfinder');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/petfinder');
 
 export const db = async (): Promise<typeof mongoose.connection> => {
     try {
