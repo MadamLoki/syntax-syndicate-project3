778af8815178841d800a494a437d09cbbc872c11
Sara <lokifeathers@gmail.com>
2025-02-12 16:23:32 -0500

    fix typeDefs

--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -18,18 +18,65 @@ const typeDefs = `
         password: String!
     }
 
+    type Pet {
+        id: ID!
+        name: String!
+        breed: String
+        age: Int
+        images: [String]
+        status: String  
+        shelterId: ID!
+    }
+
+    input CreatePetInput {
+        name: String!
+        breed: String
+        age: Int
+        images: [String]
+        status: String
+    }
+
+    input UpdatePetInput {
+        name: String
+        breed: String
+        age: Int
+        images: [String]
+        status: String
+    }
+
+    type Application {
+        id: ID!
+        petId: ID!
+        adopterId: ID
+        message: String!
+        status: String!
+        createdAt: String!
+    }
+
+    input CreateApplicationInput {
+        petId: ID!
+        message: String!
+    }
+
     type Query {
         profiles: [Profile]!
         profile(profileId: ID!): Profile
         me: Profile
+        pets: [Pet]!
+        pet(id: ID!): Pet
+        applications: [Application]!
+        application(id: ID!): Application
     }
 
     type Mutation {
         addProfile(input: ProfileInput!): Auth
         login(email: String!, password: String!): Auth
         removeProfile: Profile
+        createPet(input: CreatePetInput!): Pet!
+        updatePet(id: ID!, input: UpdatePetInput!): Pet!
+        deletePet(id: ID!): Boolean!
+        createApplication(input: CreateApplicationInput!): Application!
     }
 `;
 
 export default typeDefs;
-
