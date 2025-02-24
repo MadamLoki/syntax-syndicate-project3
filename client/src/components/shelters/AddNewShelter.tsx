e6b09edeed229f56bb0048a08db8893e0e491669
Shelia <slbradford4@yahoo.com>
2025-02-24 16:52:32 -0600

    shelters css

--- client/src/components/shelters/AddNewShelter.tsx
+++ client/src/components/shelters/AddNewShelter.tsx
@@ -40,25 +40,25 @@ const AddNewShelter = () => {
             }
         }).then(response => {
             console.log("Shelter created successfully:", response.data);
-          }).catch(error => {
+        }).catch(error => {
             console.error("Error creating shelter:", error);
-          });
+        });
     }
 
     return (
-        <div className="new-shelters">
-            <h2>Add new shelter</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="location">Location:</label>
-                    <input type="text" id="location" name="location" placeholder="1600 Amphitheatre Parkway, Mountain View, CA" onChange={handleChangeInput} />
+        <div className="max-w-md mx-auto space-y-8">
+            <h2 className="text-3xl font-bold text-center">Add new shelter</h2>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 justify-center">
+                <div className="mb-4">
+                    <label htmlFor="location" className="mb-2 block text-xs font-semibold">Location:</label>
+                    <input type="text" id="location" name="location" className="w-full pl-4 pr-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" placeholder="1600 Amphitheatre Parkway, Mountain View, CA" onChange={handleChangeInput} />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="contact" className="mb-2 block text-xs font-semibold">Email</label>
+                    <input type="text" id="contact" name="contactInfo" className="w-full pl-4 pr-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" placeholder="johndoe@email.com" onChange={handleChangeInput} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="contact">Email</label>
-                    <input type="text" id="contact" name="contactInfo" placeholder="johndoe@email.com" onChange={handleChangeInput} />
-                </div>
-                <div className="form-group">
-                    <button type="submit">Submit</button>
+                    <button className="bg-blue-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors" type="submit">Submit</button>
                 </div>
             </form>
         </div>
