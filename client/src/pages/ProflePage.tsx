c4bf3b2e6263e1a25d35de49578fc1dd98323d1f
Sara <lokifeathers@gmail.com>
2025-03-03 15:36:47 -0500

    fix: import React in ProfilePage and update tsconfig for JSX

--- client/src/pages/ProflePage.tsx
+++ client/src/pages/ProflePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { gql } from '@apollo/client';
 import { jwtDecode } from 'jwt-decode';
